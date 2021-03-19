46ed549bc2e8143ab55d4e26061dc2f6c034c965
Shawn Muldrew <shawnmuldrew@gmail.com>
2021-03-18 22:00:40 -0600

    feat: improve fact display
    Display the four different fact types correctly.

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-    // Import 
+    // Import dinosaur data 
     import {dinosaurs} from './dino.js';
-    console.log(dinosaurs);
-    console.log(dinosaurs.Dinos[0].species);
+
+    // Hide grid
     const gridElement = document.getElementById('grid');
     gridElement.style.display = "none";
-    let dinosaurList = buildDinosaurs();
 
+    // Function to capitalize string
+    const capitalize = (str) => {
+        return str.charAt(0).toUpperCase() + str.slice(1)
+      }
     
     // Create Dino Constructor
     function Dinosaur(species, weight, height, diet, where, when, fact, image) {
@@ -21,24 +24,28 @@
         this.compareWeight = function(weight) {
             if (weight > this.weight) {
                 return 'This dino weights less than you!';
-            }  else {
+            }  else if (weight < this.weight) {
                 return 'This dino weights more than you!';
+            }  else {
+                return 'You weight the same as this dino!!';
             }
         };
         // Create Dino Compare Method 2
         this.compareHeight = function(height) {
             if (height > this.height) {
                 return 'This dino is shorter than you!';
-            }  else {
+            }  else if (height < this.height) {
                 return 'This dino is taller than you!';
+            }  else {
+                return 'You are the same height as this dino!!'
             }
         };
         // Create Dino Compare Method 3
         this.compareDiet = function(diet) {
-            if (diet === this.diet) {
+            if (diet.toUpperCase() === this.diet.toUpperCase()) {
                 return `You are both ${diet}s`;
             }  else {   
-                return `You are a ${diet} and this dino is a ${this.diet}`;
+                return `You are a ${diet} and this dino is a ${capitalize(this.diet)}`;
             }
         };
     }
@@ -52,6 +59,8 @@
         return dinoArray;
     }
 
+    let dinosaurList = buildDinosaurs();
+
     // Create Human Object
     function Human(name, weight, height, diet, image) {
         this.name = name;
@@ -60,10 +69,10 @@
         this.diet = diet;
         this.image = image;
     }
+
     let human = new Human(null, null, null, null, 'images/human.png')
 
-    // Use IIFE to get human data from form
-    
+    // Use IIFE to get human data from form    
     document.getElementById('btn').addEventListener('click', function() {
         (function (human) {
             human.name = document.getElementById('name').value;
@@ -73,27 +82,19 @@
             human.weight = parseFloat(document.getElementById('weight').value);
             human.diet = document.getElementById('diet').value;
         })(human);
-        // console.log(human.name);
-        // console.log(human.height);
-        // console.log(human.weight);
-        // console.log(human.diet);
-
-        console.log(dinosaurList[0].compareWeight(human.weight));
-        console.log(dinosaurList[0].compareHeight(human.height));
-        console.log(dinosaurList[0].compareDiet(human.diet));
-
         
         // Generate Tiles for each Dino in Array
-        const gridElement = document.getElementById('grid');
         dinosaurList.forEach((item, index) => {
+            // Make sure human tile gets put in middle of grid
             if (index === 4) {
                 let div = document.createElement('div');
-                div.innerHTML = human.name + '.<br />' + '<img src="'+human.image+'">' + human.diet;
+                div.innerHTML = human.name + '<br />' + '<img src="'+human.image+'">' + human.diet;
                 div.setAttribute('class', 'grid-item');
                 gridElement.appendChild(div); 
             }
             let div = document.createElement('div');
-            let factNumber = Math.floor(Math.random() * 4) + 1; 
+            let factNumber = Math.floor(Math.random() * 4) + 1;
+            // Make sure Pigeon displays given fact (no comparison facts for Pigeon)
             if (item.species === 'Pigeon') {
                 factNumber = 1;
             }
